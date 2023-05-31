b1d3f4be78804c81b5ceec6d60068e3bfcd9ecdb
lLawlietl <becky_b_big@hotmail.com>
2023-05-31 10:11:29 +0700

    update

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/api", (req, res) => {
+  const path = `/api/item`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get("/api/item/:slug", (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
