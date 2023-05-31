af9b3538cb9545fe3ea627fc35aaa6daec9207ff
lLawlietl <becky_b_big@hotmail.com>
2023-05-31 11:06:55 +0700

    update

--- src/controllers/line-bot/webhook.js
+++ src/controllers/line-bot/webhook.js
@@ -13,8 +13,8 @@ const line = require("@line/bot-sdk");
 let channelAccessToken = process.env.CHANNEL_ACCESS_TOKEN;
 let channelSecret = process.env.CHANNEL_SECRET;
 const client = new line.Client({
-    "channelAccessToken": channelAccessToken,
-    "channelSecret": channelSecret
+  channelAccessToken: channelAccessToken,
+  channelSecret: channelSecret,
 });
 
 const webhook = (req, res) => {
@@ -29,61 +29,61 @@ const webhook = (req, res) => {
         if (type === "text") {
           console.log("TEXT");
           console.log(text.trim());
-        //   let register = "ลงทะเบียนโครงการ :";
-        //   let report = "ส่งคำร้อง";
-        //   if (
-        //     text.trim().toLowerCase() === "smart support" ||
-        //     words.words.includes(text.trim())
-        //   ) {
-        //     var groupId = event.source.groupId;
-        //     if (!groupId) {
-        //       return client.replyMessage(replyToken, {
-        //         type: "text",
-        //         text: "รับรองการใช้งานแบบกลุ่มเท่านั้น",
-        //       });
-        //       // return client.replyMessage(replyToken, notIngroup);
-        //     } else {         
-        //       var detail = await getDetailByGroupId.getDetailByGroupId(groupId);
-        //       if (detail) {
-        //         var userId = event.source.userId;
-        //         var uriReportProblem = configJS.URL_LIFF_REPORTPROBLEM;
-        //         var uri = uriReportProblem + "?user=" + userId;
-        //         replyRegisterProject.contents.contents[0].hero.action.uri = uri;
-        //         replyRegisterProject.contents.contents[0].footer.contents[0].action.uri =
-        //           uri;
+          let register = "ลงทะเบียนโครงการ :";
+          let report = "ส่งคำร้อง";
+          //   if (
+          //     text.trim().toLowerCase() === "smart support" ||
+          //     words.words.includes(text.trim())
+          //   ) {
+          //     var groupId = event.source.groupId;
+          //     if (!groupId) {
+          //       return client.replyMessage(replyToken, {
+          //         type: "text",
+          //         text: "รับรองการใช้งานแบบกลุ่มเท่านั้น",
+          //       });
+          //       // return client.replyMessage(replyToken, notIngroup);
+          //     } else {
+          //       var detail = await getDetailByGroupId.getDetailByGroupId(groupId);
+          //       if (detail) {
+          //         var userId = event.source.userId;
+          //         var uriReportProblem = configJS.URL_LIFF_REPORTPROBLEM;
+          //         var uri = uriReportProblem + "?user=" + userId;
+          //         replyRegisterProject.contents.contents[0].hero.action.uri = uri;
+          //         replyRegisterProject.contents.contents[0].footer.contents[0].action.uri =
+          //           uri;
 
-        //         if (words.words.includes(text.trim())) {
-        //           reportLog(groupId, userId, text.trim());
-        //         }
+          //         if (words.words.includes(text.trim())) {
+          //           reportLog(groupId, userId, text.trim());
+          //         }
 
-        //         return client.replyMessage(replyToken, replyRegisterProject);
-        //       } else {
-        //         var userId = event.source.userId;
-        //         var uriRegister = configJS.URL_LIFF_REGISTER;
-        //         var uri = uriRegister + "?user=" + userId;
-        //         noRegister.contents.contents[0].hero.action.uri = uri;
-        //         noRegister.contents.contents[0].body.action.uri = uri;
-        //         noRegister.contents.contents[0].footer.contents[0].action.uri =
-        //           uri;
-                  
-        //         return client.replyMessage(replyToken, noRegister);
-        //       }
-        //     }
-        //   } else if (text.trim().includes(register)) {
-        //     let responRegister = {
-        //       type: "text",
-        //       text: "ลงทะเบียนโครงการสำเร็จ",
-        //     };
-        //     return client.replyMessage(replyToken, responRegister);
-        //   } else if (text.trim().includes(report)){
-        //     let categoryName = text.split(" ")[2];
-        //     let messageResponse = `ขอบคุณท่านคณะกรรมการที่ไว้วางใจบริษัท SMART\r\rทีมส่วนกลางได้รับข้อมูลของท่านแล้ว เรื่อง "${categoryName}"\r\rข้อมูลของท่านจะส่งตรงไปถึงผู้บริหารและเราจะดำเนินการอย่างเร็วที่สุด`;
-        //     let responReport = {
-        //       type: "text",
-        //       text: messageResponse,
-        //     };
-        //     return client.replyMessage(replyToken, responReport);
-        //   }
+          //         return client.replyMessage(replyToken, replyRegisterProject);
+          //       } else {
+          //         var userId = event.source.userId;
+          //         var uriRegister = configJS.URL_LIFF_REGISTER;
+          //         var uri = uriRegister + "?user=" + userId;
+          //         noRegister.contents.contents[0].hero.action.uri = uri;
+          //         noRegister.contents.contents[0].body.action.uri = uri;
+          //         noRegister.contents.contents[0].footer.contents[0].action.uri =
+          //           uri;
+
+          //         return client.replyMessage(replyToken, noRegister);
+          //       }
+          //     }
+          //   } else if (text.trim().includes(register)) {
+          //     let responRegister = {
+          //       type: "text",
+          //       text: "ลงทะเบียนโครงการสำเร็จ",
+          //     };
+          //     return client.replyMessage(replyToken, responRegister);
+          //   } else if (text.trim().includes(report)){
+          //     let categoryName = text.split(" ")[2];
+          //     let messageResponse = `ขอบคุณท่านคณะกรรมการที่ไว้วางใจบริษัท SMART\r\rทีมส่วนกลางได้รับข้อมูลของท่านแล้ว เรื่อง "${categoryName}"\r\rข้อมูลของท่านจะส่งตรงไปถึงผู้บริหารและเราจะดำเนินการอย่างเร็วที่สุด`;
+          //     let responReport = {
+          //       type: "text",
+          //       text: messageResponse,
+          //     };
+          //     return client.replyMessage(replyToken, responReport);
+          //   }
         }
       }
 
@@ -92,6 +92,7 @@ const webhook = (req, res) => {
           type: "text",
           text: "สวัสดีครับ SMART SUPPORT มาแล้วครับ พิมพ์ SMART SUPPORT เพื่อเรียกใช้งานได้เลยครับ",
         });
+      }
     })
   )
     .then(() => res.end())
