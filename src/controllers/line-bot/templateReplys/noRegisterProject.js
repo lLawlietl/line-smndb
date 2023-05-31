610dcf71f53f89510055ea1e443d58e41513f47b
lLawlietl <becky_b_big@hotmail.com>
2023-05-31 15:41:57 +0700

    sample image

--- src/controllers/line-bot/templateReplys/noRegisterProject.js
+++ src/controllers/line-bot/templateReplys/noRegisterProject.js
@@ -9,73 +9,28 @@ const replyRegisterProject = {
         type: "bubble",
         action: {
           type: "uri",
-          uri: "https://www.figma.com/file/q30nTaySS3PGRooKNNz0lD/Juristic-Chatbot?node-id=256:9486",
+          uri: "https://smndb.vercel.app/images/cards/alleluia/014.jpg",
         },
         hero: {
           type: "image",
-          url: "https://www.img.in.th/images/6495a53f4ec2119d22f4e076725d5c2f.png",
+          url: "https://smndb.vercel.app/images/cards/alleluia/014.jpg",
           size: "full",
-          aspectRatio: "20:13",
-          aspectMode: "cover",
-          action: {
-            type: "uri",
-            label: "Action",
-            // uri: "https://liff.line.me/1657030801-EodXR5gO?user=book",
-          },
+          aspectRatio: "10:15",
+          aspectMode: "fit",
         },
-        body: {
-          type: "box",
-          layout: "vertical",
-          spacing: "sm",
-          action: {
-            type: "uri",
-            label: "Action",
-            // uri: "https://liff.line.me/1657030801-EodXR5gO?user=book",
-          },
-          contents: [
-            {
-              type: "text",
-              text: "สมัครโครงการ",
-              weight: "bold",
-              size: "lg",
-              wrap: true,
-              contents: [],
-            },
-            {
-              type: "box",
-              layout: "baseline",
-              contents: [
-                {
-                  type: "text",
-                  text: "สมัครโครการเพื่อลงทะเบียน",
-                  weight: "regular",
-                  size: "md",
-                  color: "#6B6666FF",
-                  flex: 0,
-                  wrap: true,
-                  contents: [],
-                },
-              ],
-            },
-          ],
+      },
+      {
+        type: "bubble",
+        action: {
+          type: "uri",
+          uri: "https://smndb.vercel.app/images/cards/alleluia/015.jpg",
         },
-        footer: {
-          type: "box",
-          layout: "vertical",
-          spacing: "sm",
-          contents: [
-            {
-              type: "button",
-              action: {
-                type: "uri",
-                label: "ลงทะเบียน",
-                // uri: "https://liff.line.me/1657030801-EodXR5gO?user=book",
-              },
-              color: "#F6AF0DFF",
-              style: "primary",
-              gravity: "top",
-            },
-          ],
+        hero: {
+          type: "image",
+          url: "https://smndb.vercel.app/images/cards/alleluia/015.jpg",
+          size: "full",
+          aspectRatio: "10:15",
+          aspectMode: "fit",
         },
       },
     ],
