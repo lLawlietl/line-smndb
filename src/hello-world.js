2a9c8dcd4196d33b359f91ed3e6f412e04a71cfa
lLawlietl <becky_b_big@hotmail.com>
2023-06-06 11:41:10 +0700

    add image

--- src/hello-world.js
+++ src/hello-world.js
@@ -1,7 +1,39 @@
 const http = require("node:http");
+const axios = require("axios");
+const cards = require("./data.json");
 
 const hostname = "127.0.0.1";
 const port = 3000;
+
+let filter = cards.filter((o) => o.element == "earth");
+console.log("filter >>", filter);
+
+// let dataD4K = [];
+
+// var api = {
+//   method: "get",
+//   url: "https://smndb.vercel.app/data/d4k",
+// };
+
+// axios(api)
+//   .then(function (response) {
+//     //console.log("dataD4K >>", response.data);
+//     const result = response.data;
+
+//     var strippedBody = body.replace(/<!--[\s\S]*?-->/g, "")
+
+//         console.log(strippedBody);
+
+//         var $ = cheerio.load(strippedBody),
+//         post = $(".text_exposed_root p").text();
+
+//     let text = document.getElementById("root").textContent;
+//     console.log("dataD4K3 >>", text);
+//     dataD4K = result.data;
+//   })
+//   .catch(function (error) {
+//     console.log("error >>", error);
+//   });
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
