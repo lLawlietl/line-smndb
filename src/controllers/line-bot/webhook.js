314aa9f821bdf45d27ae24838ad4be8456db2717
lLawlietl <becky_b_big@hotmail.com>
2023-06-06 16:43:01 +0700

    test

--- src/controllers/line-bot/webhook.js
+++ src/controllers/line-bot/webhook.js
@@ -332,100 +332,27 @@ const webhook = (req, res) => {
 
             const xxx = {
               type: "bubble",
+              header: {
+                type: "box",
+                layout: "vertical",
+                contents: [
+                  {
+                    type: "text",
+                    text: "Header text",
+                  },
+                ],
+              },
               hero: {
                 type: "image",
-                url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png",
-                size: "full",
-                aspectRatio: "20:13",
-                aspectMode: "cover",
-                action: {
-                  type: "uri",
-                  label: "Action",
-                  uri: "https://linecorp.com",
-                },
+                url: "https://example.com/flex/images/image.jpg",
               },
               body: {
                 type: "box",
                 layout: "vertical",
-                spacing: "md",
-                action: {
-                  type: "uri",
-                  label: "Action",
-                  uri: "https://linecorp.com",
-                },
                 contents: [
                   {
                     type: "text",
-                    text: "Brown's Burger",
-                    weight: "bold",
-                    size: "xl",
-                    contents: [],
-                  },
-                  {
-                    type: "box",
-                    layout: "vertical",
-                    spacing: "sm",
-                    contents: [
-                      {
-                        type: "box",
-                        layout: "baseline",
-                        contents: [
-                          {
-                            type: "icon",
-                            url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/restaurant_regular_32.png",
-                          },
-                          {
-                            type: "text",
-                            text: "$10.5",
-                            weight: "bold",
-                            margin: "sm",
-                            contents: [],
-                          },
-                          {
-                            type: "text",
-                            text: "400kcl",
-                            size: "sm",
-                            color: "#AAAAAA",
-                            align: "end",
-                            contents: [],
-                          },
-                        ],
-                      },
-                      {
-                        type: "box",
-                        layout: "baseline",
-                        contents: [
-                          {
-                            type: "icon",
-                            url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/restaurant_large_32.png",
-                          },
-                          {
-                            type: "text",
-                            text: "$15.5",
-                            weight: "bold",
-                            flex: 0,
-                            margin: "sm",
-                            contents: [],
-                          },
-                          {
-                            type: "text",
-                            text: "550kcl",
-                            size: "sm",
-                            color: "#AAAAAA",
-                            align: "end",
-                            contents: [],
-                          },
-                        ],
-                      },
-                    ],
-                  },
-                  {
-                    type: "text",
-                    text: "Sauce, Onions, Pickles, Lettuce & Cheese",
-                    size: "xxs",
-                    color: "#AAAAAA",
-                    wrap: true,
-                    contents: [],
+                    text: "Body text",
                   },
                 ],
               },
@@ -434,20 +361,13 @@ const webhook = (req, res) => {
                 layout: "vertical",
                 contents: [
                   {
-                    type: "spacer",
-                    size: "xxl",
-                  },
-                  {
-                    type: "button",
-                    action: {
-                      type: "uri",
-                      label: "Add to Cart",
-                      uri: "https://linecorp.com",
-                    },
-                    color: "#905C44",
-                    style: "primary",
+                    type: "text",
+                    text: "Footer text",
                   },
                 ],
+              },
+              styles: {
+                comment: "See the example of a bubble style object",
               },
             };
 
